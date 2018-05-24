4c380cabab6129ce7151567226646b2325e0a17d
Steve Huff <steve.huff@runkeeper.com>
2018-05-24 13:05:57 -0400

    feat: add a stack to accumulate redirects

--- main.go
+++ main.go
@@ -7,21 +7,24 @@ import (
   "net/url"
   "os"
 
+  "github.com/golang-collections/collections/stack"
   "github.com/jcelliott/lumber"
   "gopkg.in/urfave/cli.v1" // imports as package "cli"
 )
+
+var redirectsStack *stack.Stack = stack.New()
 
 type parsedResponse struct {
   code int
   request string
   response string
   content string
+  redirects *stack.Stack
 }
 
 func main() {
   lumber.Info("initialized")
 
-  // redirects := make([]*url.URL, 0)
 
   app := cli.NewApp()
   app.EnableBashCompletion = true
@@ -48,7 +51,7 @@ func main() {
 }
 
 func doAction(arg string) (parsedResponse, error) {
-  lumber.Info("%s\n", arg)
+  lumber.Info("arg: %s\n", arg)
   // try to parse the arg as a url
   parsedUrl, err := url.Parse(arg)
 
@@ -70,22 +73,25 @@ func getUrl(u *url.URL) (parsedResponse, error) {
   request := resp.Request.URL.String()
   response := resp.Header.Get("location")
   content := ""
+  redirects := redirectsStack
 
   defer resp.Body.Close()
 
-  return parsedResponse{code: code, request: request, response:response, content: content}, err
+  return parsedResponse{code: code, request: request, response:response, content: content, redirects: redirects}, err
 }
 
 type checkRedirectFunction func(*http.Request, []*http.Request) error
 
 func checkRedirect() checkRedirectFunction {
   return func(req *http.Request, via []*http.Request) error {
-    return http.ErrUseLastResponse
+    lumber.Info("redirected to: %s\n", req.URL.String())
+    redirectsStack.Push(req.URL)
+    return nil
   }
 }
 
 func printResp(resp parsedResponse) error {
-  fmt.Printf("%d\t%s\t%s\n", resp.code, resp.request, resp.response)
+  fmt.Printf("%d\t%s\t%s\t%d\n", resp.code, resp.request, resp.response, resp.redirects.Len())
 
   return nil
 }
